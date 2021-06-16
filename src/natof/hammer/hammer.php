56c1eeb913656ecd465e1d638511643ec7d96c84
natof <80003956+natof@users.noreply.github.com>
2021-06-16 16:27:29 -0400

    Update hammer.php

--- src/natof/hammer/hammer.php
+++ src/natof/hammer/hammer.php
@@ -9,9 +9,12 @@ class hammer extends PluginBase{
 
     private static $instance;
 
+    public static $config;
+    
     public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents(new EventListener(), $this);
         $this->saveResource("config.yml");
+        self::$config = $this->getConfig();
     }
 
 
